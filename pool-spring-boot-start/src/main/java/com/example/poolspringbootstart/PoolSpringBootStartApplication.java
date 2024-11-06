a15fdb0c8e08d649f9d4d1357fbb1f7c49b23cf8
jiangzhuoying <1768017757@qq.com>
2024-11-07 01:50:38 +0800

    feat: 初始化工程

---
+++ pool-spring-boot-start/src/main/java/com/example/poolspringbootstart/PoolSpringBootStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.poolspringbootstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolSpringBootStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoolSpringBootStartApplication.class, args);
+    }
+
+}
---
+++ pool-spring-boot-start/src/test/java/com/example/poolspringbootstart/PoolSpringBootStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poolspringbootstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoolSpringBootStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
